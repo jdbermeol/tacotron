989dc85b84efd0688ad62f512a6d956562ec78f7
Jose Bermeo <josedavid@bunnyinc.com>
2017-08-16 09:22:55 -0500

    Allow unicode chars

--- hyperparams.py
+++ hyperparams.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #/usr/bin/python2
 '''
-By kyubyong park. kbpark.linguist@gmail.com. 
+By kyubyong park. kbpark.linguist@gmail.com.
 https://www.github.com/kyubyong/tacotron
 '''
 
@@ -9,13 +9,14 @@ class Hyperparams:
     '''Hyper parameters'''
     # mode
     sanity_check = True
-    
+
     # data
-    text_file = 'WEB/text.csv'
-    sound_fpath = 'WEB'
+    text_file = 'dataset/text.csv'
+    sound_fpath = 'dataset'
+    vocab_file = 'dataset/vocab.csv'
     max_len = 100 if not sanity_check else 30 # maximum length of text
     min_len = 10 if not sanity_check else 20 # minimum length of text
-    
+
     # signal processing
     sr = 22050 # Sampling rate. Paper => 24000
     n_fft = 2048 # fft points (samples)
@@ -25,9 +26,9 @@ class Hyperparams:
     win_length = int(sr*frame_length) # samples This is dependent on the frame_length.
     n_mels = 80 # Number of Mel banks to generate
     power = 1.2 # Exponent for amplifying the predicted magnitude
-    n_iter = 30 # Number of inversion iterations 
+    n_iter = 30 # Number of inversion iterations
     use_log_magnitude = True # if False, use magnitude
-    
+
     # model
     embed_size = 256 # alias = E
     encoder_num_banks = 16
@@ -35,7 +36,7 @@ class Hyperparams:
     num_highwaynet_blocks = 4
     r = 5 # Reduction factor. Paper => 2, 3, 5
     norm_type = 'ins'  # a normalizer function. value: bn, ln, ins, or None
-    
+
     # training scheme
     lr = 0.0005 # Paper => Exponential decay
     logdir = "logdir" if not sanity_check else "logdir_s"
@@ -44,9 +45,9 @@ class Hyperparams:
     num_epochs = 10000 if not sanity_check else 40 # Paper => 2M global steps!
     loss_type = "l2" # Or you can test "l2"
     num_samples = 32
-    
+
     # etc
     num_gpus = 1 # If you have multiple gpus, adjust this option, and increase the batch size
                  # and run `train_multiple_gpus.py` instead of `train.py`.
-    target_zeros_masking = False # If True, we mask zero padding on the target, 
-                                 # so exclude them from the loss calculation.     
+    target_zeros_masking = False # If True, we mask zero padding on the target,
+                                 # so exclude them from the loss calculation.
--- prepro.py
+++ prepro.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 '''
-By kyubyong park. kbpark.linguist@gmail.com. 
+By kyubyong park. kbpark.linguist@gmail.com.
 https://www.github.com/kyubyong/tacotron
 '''
 
@@ -14,30 +14,46 @@ import re
 from hyperparams import Hyperparams as hp
 import numpy as np
 
+def store_vocab(idx2char):
+    writer = csv.writer(codecs.open(hp.vocab_file, 'w', 'utf-8'))
+    for idx in idx2char:
+        writer.writerow([idx, idx2char[idx].encode('unicode-escape')])
 
-def load_vocab():
-    vocab = "EG abcdefghijklmnopqrstuvwxyz'" # E: Empty. ignore G
+
+def learn_vocab():
+    reader = csv.reader(codecs.open(hp.text_file, 'r', 'utf-8'))
+    vocab = set()
+    for sound_fname, text in reader:
+        vocab = vocab.union(set(list(text.decode('unicode-escape'))))
     char2idx = {char:idx for idx, char in enumerate(vocab)}
     idx2char = {idx:char for idx, char in enumerate(vocab)}
-    return char2idx, idx2char    
+    return char2idx, idx2char
+
+def load_vocab():
+    reader = csv.reader(codecs.open(hp.vocab_file, 'r', 'utf-8'))
+    char2idx = {}
+    idx2char = {}
+    for idx, char in reader:
+        char2idx[char] = idx
+        idx2char[idx] = char
+    return char2idx, idx2char
 
 def create_train_data():
     # Load vocabulary
-    char2idx, idx2char = load_vocab() 
-      
+    char2idx, idx2char = load_vocab()
+
     texts, sound_files = [], []
     reader = csv.reader(codecs.open(hp.text_file, 'rb', 'utf-8'))
-    for row in reader:
-        sound_fname, text, duration = row
+    for sound_fname, text in reader:
         sound_file = hp.sound_fpath + "/" + sound_fname + ".wav"
-        text = re.sub(r"[^ a-z']", "", text.strip().lower())
-         
+        text = text.decode('unicode-escape')
+
         if hp.min_len <= len(text) <= hp.max_len:
-            texts.append(np.array([char2idx[char] for char in text], np.int32).tostring())
+            texts.append(np.array([char2idx[char] for char in text if char in char2idx], np.int32).tostring())
             sound_files.append(sound_file)
-             
+
     return texts, sound_files
-     
+
 def load_train_data():
     """We train on the whole data but the last num_samples."""
     texts, sound_files = create_train_data()
@@ -46,7 +62,7 @@ def load_train_data():
     else:
         texts, sound_files = texts[:-hp.num_samples], sound_files[:-hp.num_samples]
     return texts, sound_files
- 
+
 def load_eval_data():
     """We evaluate on the last num_samples."""
     texts, _ = create_train_data()
@@ -54,12 +70,10 @@ def load_eval_data():
         texts = texts[:hp.batch_size]
     else:
         texts = texts[-hp.num_samples:]
-    
+
     X = np.zeros(shape=[len(texts), hp.max_len], dtype=np.int32)
     for i, text in enumerate(texts):
-        _text = np.fromstring(text, np.int32) # byte to int 
+        _text = np.fromstring(text, np.int32) # byte to int
         X[i, :len(_text)] = _text
-    
-    return X
- 
 
+    return X
